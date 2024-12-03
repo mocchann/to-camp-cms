b7c49cadc618b703783257c269eaf51e1720689b
もっちゃん <s7056i3056@gmail.com>
2024-12-03 23:25:30 +0900

    feat: create campground entity

---
+++ app/Domain/Models/CampGrounds/CampGround.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Domain\Models\CampGrounds;
+
+class CampGround
+{
+    public function __construct(
+        private readonly CampGroundId $id,
+        private CampGroundName $name,
+        private CampGroundAddress $address,
+        private CampGroundPrice $price,
+        private CampGroundImage $image,
+        private CampGroundStatus $status
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->address = $address;
+        $this->price = $price;
+        $this->image = $image;
+        $this->status = $status;
+    }
+
+    public function getId(): CampGroundId
+    {
+        return $this->id;
+    }
+
+    public function getName(): CampGroundName
+    {
+        return $this->name;
+    }
+
+    public function getAddress(): CampGroundAddress
+    {
+        return $this->address;
+    }
+
+    public function getPrice(): CampGroundPrice
+    {
+        return $this->price;
+    }
+
+    public function getImage(): CampGroundImage
+    {
+        return $this->image;
+    }
+
+    public function getStatus(): CampGroundStatus
+    {
+        return $this->status;
+    }
+}
