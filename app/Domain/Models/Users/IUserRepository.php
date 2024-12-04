80381c8e8c471fb08e07474ab66d768cdb199397
もっちゃん <s7056i3056@gmail.com>
2024-12-05 00:06:40 +0900

    add: repositoryにregisterメソッド追加

--- app/Domain/Models/Users/IUserRepository.php
+++ app/Domain/Models/Users/IUserRepository.php
@@ -7,4 +7,5 @@ use App\Domain\Users\User;
 interface IUserRepository
 {
     public function findById(UserId $id): ?User;
+    public function register(int $id, string $name, string $email, string $password): void;
 }
