b61b0aba8f6a7713b3b7da230980191bf8346965
もっちゃん <s7056i3056@gmail.com>
2024-12-12 23:59:50 +0900

    add: hash

--- app/Domain/Models/Users/UserPassword.php
+++ app/Domain/Models/Users/UserPassword.php
@@ -2,11 +2,13 @@
 
 namespace App\Domain\Models\Users;
 
+use Illuminate\Support\Facades\Hash;
+
 class UserPassword
 {
     public function __construct(private string $password)
     {
-        $this->password = $password;
+        $this->password = Hash::make($password);
     }
 
     public function getValue(): string
