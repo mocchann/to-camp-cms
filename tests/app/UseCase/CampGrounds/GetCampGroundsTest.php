39a927c9e995a84a334fe60065712b12bb983213
もっちゃん <s7056i3056@gmail.com>
2024-12-24 23:00:00 +0900

    fix: typo

---
+++ tests/app/UseCase/CampGrounds/GetCampGroundsTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\App\UseCase\CampGrounds;
+
+use App\Domain\Models\CampGrounds\CampGround;
+use App\Domain\Models\CampGrounds\CampGroundAddress;
+use App\Domain\Models\CampGrounds\CampGroundId;
+use App\Domain\Models\CampGrounds\CampGroundImage;
+use App\Domain\Models\CampGrounds\CampGroundName;
+use App\Domain\Models\CampGrounds\CampGroundPrice;
+use App\Domain\Models\CampGrounds\CampGroundStatus;
+use App\Domain\Models\CampGrounds\ICampGroundRepository;
+use App\UseCase\CampGrounds\GetCampGrounds;
+use Mockery;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class GetCampGroundsTest extends TestCase
+{
+    #[Test]
+    public function キャンプ場一覧を取得できる(): void
+    {
+        $camp_grounds = [
+            new CampGround(
+                new CampGroundId(1),
+                new CampGroundName('テストオートキャンプ場'),
+                new CampGroundAddress('沖縄県晴海町1-12-89'),
+                new CampGroundPrice(3000),
+                new CampGroundImage('https://example.com/image.jpg'),
+                new CampGroundStatus('open'),
+            ),
+            new CampGround(
+                new CampGroundId(2),
+                new CampGroundName('テストキャンプ場'),
+                new CampGroundAddress('北海道青天町807'),
+                new CampGroundPrice(5000),
+                new CampGroundImage('https://example.com/image.jpg'),
+                new CampGroundStatus('open'),
+            ),
+        ];
+
+        $repository = Mockery::mock(ICampGroundRepository::class);
+        $repository->shouldReceive('get')->andReturn($camp_grounds);
+
+        /** @var ICampGroundRepository $repository */
+        $use_case = new GetCampGrounds($repository);
+
+        $result = $use_case->execute();
+
+        $this->assertEquals(
+            [
+                [
+                    'id' => 1,
+                    'name' => 'テストオートキャンプ場',
+                    'address' => '沖縄県晴海町1-12-89',
+                    'price' => 3000,
+                    'image' => 'https://example.com/image.jpg',
+                    'status' => 'open',
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'テストキャンプ場',
+                    'address' => '北海道青天町807',
+                    'price' => 5000,
+                    'image' => 'https://example.com/image.jpg',
+                    'status' => 'open',
+                ],
+            ],
+            array_map(function (CampGround $camp_ground) {
+                return [
+                    'id' => $camp_ground->getId()->getValue(),
+                    'name' => $camp_ground->getName()->getValue(),
+                    'address' => $camp_ground->getAddress()->getValue(),
+                    'price' => $camp_ground->getPrice()->getValue(),
+                    'image' => $camp_ground->getImage()->getValue(),
+                    'status' => $camp_ground->getStatus()->getValue(),
+                ];
+            }, $result)
+        );
+    }
+}
---
+++ tests/app/UseCase/Users/UserLoginTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\App\UseCase\Users;
+
+use App\Domain\Models\Users\IUserRepository;
+use App\Domain\Models\Users\User;
+use App\Domain\Models\Users\UserEmail;
+use App\Domain\Models\Users\UserId;
+use App\Domain\Models\Users\UserName;
+use App\Domain\Models\Users\UserPassword;
+use App\UseCase\Users\UserLogin;
+use Illuminate\Support\Facades\Hash;
+use Mockery;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class UserLoginTest extends TestCase
+{
+    #[Test]
+    public function ユーザーがログインできる(): void
+    {
+        $user = new User(
+            new UserId(1),
+            new UserName('test_user'),
+            new UserEmail('test_user@example.com'),
+            new UserPassword(Hash::make('password'))
+        );
+
+        $repository = Mockery::mock(IUserRepository::class);
+        $repository->shouldReceive('findByEmail')
+            ->andReturnUsing(function () use ($user) {
+                return $user;
+            });
+        /**
+         * @var IUserRepository $repository
+         */
+        $use_case = new UserLogin($repository);
+
+        $this->assertTrue($use_case->execute('test_user@example.com', 'password'));
+    }
+}
