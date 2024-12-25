de3a46cc93caba63f91e988ae4a91c9cb53ed558
もっちゃん <s7056i3056@gmail.com>
2024-12-25 23:00:00 +0900

    test: 一旦テストファイルを作成

---
+++ tests/app/UseCase/CampGrounds/UpdateCampGroundTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\App\UseCase\CampGrounds;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class UpdateCampGroundTest extends TestCase
+{
+    #[Test]
+    public function てすと(): void
+    {
+        $this->assertTrue(false);
+    }
+}
