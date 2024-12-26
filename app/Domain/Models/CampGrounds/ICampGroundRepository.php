0e28e39b6cd282e5375dd5803bb5895a692e2dce
もっちゃん <s7056i3056@gmail.com>
2024-12-26 23:27:34 +0900

    add: repositoryでcommandオブジェクトをuse

--- app/Domain/Models/CampGrounds/ICampGroundRepository.php
+++ app/Domain/Models/CampGrounds/ICampGroundRepository.php
@@ -4,6 +4,7 @@ namespace App\Domain\Models\CampGrounds;
 
 use App\UseCase\CampGrounds\GetCampGroundsCommand;
 use App\UseCase\CampGrounds\RegisterCampGroundCommand;
+use App\UseCase\CampGrounds\UpdateCampGroundCommand;
 
 interface ICampGroundRepository
 {
