1baf5304a55dbeb2984c4bf6edc8256329064239
もっちゃん <s7056i3056@gmail.com>
2024-12-27 09:26:19 +0900

    fix:  mockの仕方がおかしかったので修正

--- tests/app/UseCase/CampGrounds/RegisterCampGroundTest.php
+++ tests/app/UseCase/CampGrounds/RegisterCampGroundTest.php
@@ -20,7 +20,7 @@ class RegisterCampGroundTest extends TestCase
     #[Test]
     public function キャンプ場情報を新規登録できる(): void
     {
-        $expected = new CampGround(
+        $camp_ground = new CampGround(
             new CampGroundId(1),
             new CampGroundName('テストオートキャンプ場'),
             new CampGroundAddress('沖縄県晴海町1-12-89'),
@@ -30,12 +30,12 @@ class RegisterCampGroundTest extends TestCase
         );
 
         $repository = Mockery::mock(ICampGroundRepository::class);
-        $repository->shouldReceive('save')->andReturn($expected);
+        $repository->shouldReceive('save')->andReturn($camp_ground);
 
         /** @var ICampGroundRepository $repository */
         $use_case = new RegisterCampGround($repository);
 
-        $actual = $use_case->execute(
+        $result = $use_case->execute(
             1,
             'テストオートキャンプ場',
             '沖縄県晴海町1-12-89',
@@ -44,6 +44,23 @@ class RegisterCampGroundTest extends TestCase
             'open'
         );
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals(
+            [
+                'id' => 1,
+                'name' => 'テストオートキャンプ場',
+                'address' => '沖縄県晴海町1-12-89',
+                'price' => 3000,
+                'image' => 'https://example.com/image.jpg',
+                'status' => 'open',
+            ],
+            [
+                'id' => $camp_ground->getId()->getValue(),
+                'name' => $camp_ground->getName()->getValue(),
+                'address' => $camp_ground->getAddress()->getValue(),
+                'price' => $camp_ground->getPrice()->getValue(),
+                'image' => $camp_ground->getImage()->getValue(),
+                'status' => $camp_ground->getStatus()->getValue(),
+            ],
+        );
     }
 }
