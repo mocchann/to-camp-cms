b880607850e697150daaceff2401f1b1f9904c21
もっちゃん <s7056i3056@gmail.com>
2024-12-27 11:08:05 +0900

    test: キャンプ場情報削除のテスト

---
+++ tests/app/UseCase/CampGrounds/DeleteCampGroundTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\App\UseCase\CampGrounds;
+
+use App\Domain\Models\CampGrounds\CampGround;
+use App\Domain\Models\CampGrounds\CampGroundAddress;
+use App\Domain\Models\CampGrounds\CampGroundId;
+use App\Domain\Models\CampGrounds\CampGroundImage;
+use App\Domain\Models\CampGrounds\CampGroundName;
+use App\Domain\Models\CampGrounds\CampGroundPrice;
+use App\Domain\Models\CampGrounds\CampGroundStatus;
+use App\Domain\Models\CampGrounds\ICampGroundRepository;
+use App\UseCase\CampGrounds\DeleteCampGround;
+use Mockery;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class DeleteCampGroundTest extends TestCase
+{
+    #[Test]
+    public function キャンプ場情報を削除できる()
+    {
+        $camp_ground = new CampGround(
+            new CampGroundId(1),
+            new CampGroundName('テストオートキャンプ場'),
+            new CampGroundAddress('沖縄県晴海町1-12-89'),
+            new CampGroundPrice(3000),
+            new CampGroundImage('https://example.com/image.jpg'),
+            new CampGroundStatus('open'),
+        );
+
+        $repository = Mockery::mock(ICampGroundRepository::class);
+        $repository->shouldReceive('save')->andReturn($camp_ground);
+        $repository->shouldReceive('delete')->andReturnUndefined();
+        $repository->shouldReceive('find')->andReturn(null);
+
+        /** @var ICampGroundRepository $repository */
+        $delete_use_case = new DeleteCampGround($repository);
+        $delete_use_case->execute(1);
+        $get_use_case = new GetCampGround($repository);
+
+        $$this->assertNull($get_use_case->execute(1));
+    }
+}
