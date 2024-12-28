5db912bef566427148aa850332dbc1c502e4f180
もっちゃん <s7056i3056@gmail.com>
2024-12-28 15:24:16 +0900

    fix: saveの引数を修正

--- app/Domain/Models/CampGrounds/ICampGroundRepository.php
+++ app/Domain/Models/CampGrounds/ICampGroundRepository.php
@@ -2,14 +2,13 @@
 
 namespace App\Domain\Models\CampGrounds;
 
-use App\UseCase\CampGrounds\RegisterCampGroundCommand;
 use App\UseCase\CampGrounds\UpdateCampGroundCommand;
 
 interface ICampGroundRepository
 {
     public function get(GetCampGroundsFilter $filter): array;
     public function find(CampGroundId $id): ?CampGround;
-    public function save(RegisterCampGroundCommand $command): CampGround;
+    public function save(CampGround $camp_ground): CampGround;
     public function update(UpdateCampGroundCommand $command): CampGround;
     public function delete(CampGroundId $id): void;
 }
