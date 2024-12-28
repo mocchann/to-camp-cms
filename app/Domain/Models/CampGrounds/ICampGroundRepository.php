3bc3083c6a1b68017499372c450ef894e03407ca
もっちゃん <s7056i3056@gmail.com>
2024-12-28 15:14:03 +0900

    fix: repositoryには値オブジェクトを引数として渡すので修正

--- app/Domain/Models/CampGrounds/ICampGroundRepository.php
+++ app/Domain/Models/CampGrounds/ICampGroundRepository.php
@@ -2,13 +2,12 @@
 
 namespace App\Domain\Models\CampGrounds;
 
-use App\UseCase\CampGrounds\GetCampGroundsCommand;
 use App\UseCase\CampGrounds\RegisterCampGroundCommand;
 use App\UseCase\CampGrounds\UpdateCampGroundCommand;
 
 interface ICampGroundRepository
 {
-    public function get(GetCampGroundsCommand $command): array;
+    public function get(GetCampGroundsFilter $filter): array;
     public function find(CampGroundId $id): ?CampGround;
     public function save(RegisterCampGroundCommand $command): CampGround;
     public function update(UpdateCampGroundCommand $command): CampGround;
