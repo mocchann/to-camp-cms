c70083db61ba7a105d45d92744defaf560af5b21
もっちゃん <s7056i3056@gmail.com>
2025-01-01 17:18:14 +0900

    add: ulidのためのtraitを追加

--- app/Models/CampGround.php
+++ app/Models/CampGround.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class CampGround extends Model
 {
-    //
+    use HasUlids;
 }
