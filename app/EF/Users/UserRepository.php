67293ab0bfef55bf54910c5fefe2bb1b8ab9cdfc
もっちゃん <s7056i3056@gmail.com>
2025-01-03 11:20:09 +0900

    feat: findByIdを実装

--- app/EF/Users/UserRepository.php
+++ app/EF/Users/UserRepository.php
@@ -6,12 +6,25 @@ use App\Domain\Models\Users\IUserRepository;
 use App\Domain\Models\Users\User;
 use App\Domain\Models\Users\UserEmail;
 use App\Domain\Models\Users\UserId;
+use App\Domain\Models\Users\UserName;
+use App\Domain\Models\Users\UserPassword;
+use App\Models\User as ModelsUser;
 
 class UserRepository implements IUserRepository
 {
     public function findById(UserId $id): ?User
     {
-        return null;
+        $user = ModelsUser::find($id->getValue());
+        if (is_null($user)) {
+            return null;
+        }
+
+        return new User(
+            new UserId($user->id),
+            new UserName($user->name),
+            new UserEmail($user->email),
+            new UserPassword($user->password)
+        );
     }
 
     public function findByEmail(UserEmail $id): ?User
