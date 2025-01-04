3aa83633294fd45aded6b4d1410720dcdfab5781
もっちゃん <s7056i3056@gmail.com>
2025-01-04 23:57:05 +0900

    add: fillableとリレーションを定義

--- app/Models/CampGround.php
+++ app/Models/CampGround.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CampGround extends Model
 {
     use HasUlids;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'price',
+        'image_url',
+        'elevation',
+    ];
+
+    public function statuses(): BelongsToMany
+    {
+        return $this->belongsToMany(Status::class);
+    }
+
+    public function locations(): BelongsToMany
+    {
+        return $this->belongsToMany(Location::class);
+    }
 }
