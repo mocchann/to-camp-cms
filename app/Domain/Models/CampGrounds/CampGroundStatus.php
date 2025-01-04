fa5052289ae270e742fa7e554b4d2f177f6c7b42
もっちゃん <s7056i3056@gmail.com>
2025-01-04 23:56:02 +0900

    refactor: stringを受け取るようにリファクタした

--- app/Domain/Models/CampGrounds/CampGroundStatus.php
+++ app/Domain/Models/CampGrounds/CampGroundStatus.php
@@ -3,17 +3,30 @@
 namespace App\Domain\Models\CampGrounds;
 
 use App\Domain\Enums\CampGroundStatus as EnumsCampGroundStatus;
+use InvalidArgumentException;
 
 class CampGroundStatus
 {
+    private EnumsCampGroundStatus $camp_ground_status;
+
     public function __construct(
-        private EnumsCampGroundStatus $status
+        private string $status
     ) {
-        $this->status = $status;
+        $this->camp_ground_status = $this->convert($status);
+    }
+
+    private function convert(string $status): EnumsCampGroundStatus
+    {
+        return match ($status) {
+            'draft' => EnumsCampGroundStatus::DRAFT,
+            'published' => EnumsCampGroundStatus::PUBLISHED,
+            'archived' => EnumsCampGroundStatus::ARCHIVED,
+            default => throw new InvalidArgumentException('Invalid status'),
+        };
     }
 
     public function getValue(): EnumsCampGroundStatus
     {
-        return $this->status;
+        return $this->camp_ground_status;
     }
 }
