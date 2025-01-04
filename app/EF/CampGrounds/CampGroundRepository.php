908f51eebed9ff3f0b353640d484b9e55da7e353
もっちゃん <s7056i3056@gmail.com>
2025-01-04 23:59:08 +0900

    feat: リポジトリをとりあえず実装した

--- app/EF/CampGrounds/CampGroundRepository.php
+++ app/EF/CampGrounds/CampGroundRepository.php
@@ -3,15 +3,79 @@
 namespace App\EF\CampGrounds;
 
 use App\Domain\Models\CampGrounds\CampGround;
+use App\Domain\Models\CampGrounds\CampGroundAddress;
+use App\Domain\Models\CampGrounds\CampGroundElevation;
 use App\Domain\Models\CampGrounds\CampGroundId;
+use App\Domain\Models\CampGrounds\CampGroundImage;
+use App\Domain\Models\CampGrounds\CampGroundLocation;
+use App\Domain\Models\CampGrounds\CampGroundName;
+use App\Domain\Models\CampGrounds\CampGroundPrice;
+use App\Domain\Models\CampGrounds\CampGroundStatus;
 use App\Domain\Models\CampGrounds\GetCampGroundsFilter;
 use App\Domain\Models\CampGrounds\ICampGroundRepository;
+use App\Models\CampGround as ModelsCampGround;
 
 class CampGroundRepository implements ICampGroundRepository
 {
+    /**
+     * @param GetCampGroundsFilter $filter
+     * @return CampGround[]
+     */
     public function get(GetCampGroundsFilter $filter): array
     {
-        return [];
+        $query = ModelsCampGround::query();
+
+        if ($filter->getId()) {
+            $query->where('id', $filter->getId());
+        }
+
+        if ($filter->getName()) {
+            $query->where('name', 'like', "%{$filter->getName()}%");
+        }
+
+        if ($filter->getAddress()) {
+            $query->where('address', 'like', "%{$filter->getAddress()}%");
+        }
+
+        if ($filter->getPrice()) {
+            $query->where('price', $filter->getPrice());
+        }
+
+        if ($filter->getImage()) {
+            $query->where('image_url', 'like', "%{$filter->getImage()}%");
+        }
+
+        if ($filter->getStatus()) {
+            $query->whereHas('statuses', function ($query) use ($filter) {
+                $query->where('name', $filter->getStatus());
+            });
+        }
+
+        if ($filter->getLocation()) {
+            $query->whereHas('locations', function ($query) use ($filter) {
+                $query->where('name', $filter->getLocation());
+            });
+        }
+
+        if ($filter->getElevation()) {
+            $query->where('elevation', $filter->getElevation());
+        }
+
+        $camp_grounds = $query->with('statuses', 'locations')->get();
+
+        return $camp_grounds->map(
+            fn($camp_ground, $index) =>
+            new CampGround(
+                new CampGroundId($camp_ground->id),
+                new CampGroundName($camp_ground->name),
+                new CampGroundAddress($camp_ground->address),
+                new CampGroundPrice($camp_ground->price),
+                new CampGroundImage($camp_ground->image_url),
+                new CampGroundStatus($camp_ground->statuses[$index]->name),
+                new CampGroundLocation($camp_ground->locations[$index]->name),
+                new CampGroundElevation($camp_ground->elevation)
+            )
+        )->toArray();
     }
 
     public function findById(CampGroundId $id): ?CampGround
---
+++ tests/app/EF/CampGrounds/CampGroundRepositoryTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\App\EF\CampGrounds;
+
+use App\Domain\Models\CampGrounds\CampGround;
+use App\Domain\Models\CampGrounds\CampGroundAddress;
+use App\Domain\Models\CampGrounds\CampGroundElevation;
+use App\Domain\Models\CampGrounds\CampGroundId;
+use App\Domain\Models\CampGrounds\CampGroundImage;
+use App\Domain\Models\CampGrounds\CampGroundLocation;
+use App\Domain\Models\CampGrounds\CampGroundName;
+use App\Domain\Models\CampGrounds\CampGroundPrice;
+use App\Domain\Models\CampGrounds\CampGroundStatus;
+use App\Domain\Models\CampGrounds\GetCampGroundsFilter;
+use App\EF\CampGrounds\CampGroundRepository;
+use App\Models\CampGround as ModelsCampGround;
+use App\Models\Location;
+use App\Models\Status;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class CampGroundRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    #[Test]
+    public function get__キャンプ場のエンティティのリストを取得できる(): void
+    {
+        $models_camp_ground = ModelsCampGround::create([
+            'name' => 'テストオートキャンプ場',
+            'address' => 'テスト県テスト市テスト町000',
+            'price' => 1000,
+            'image_url' => 'https://example.com/test.jpg',
+            'elevation' => 1000,
+        ]);
+        $draft = Status::create(['name' => 'draft']);
+        $published = Status::create(['name' => 'published']);
+        $archived = Status::create(['name' => 'archived']);
+        $sea = Location::create(['name' => 'sea']);
+        $mountain = Location::create(['name' => 'mountain']);
+        $river = Location::create(['name' => 'river']);
+        $lake = Location::create(['name' => 'lake']);
+        $woods = Location::create(['name' => 'woods']);
+        $highland = Location::create(['name' => 'highland']);
+        $models_camp_ground->statuses()->attach($published->id);
+        $models_camp_ground->locations()->attach($mountain->id);
+        $repository = new CampGroundRepository();
+        $filter = new GetCampGroundsFilter();
+
+        $this->assertEquals(
+            [
+                new CampGround(
+                    new CampGroundId($models_camp_ground->id),
+                    new CampGroundName('テストオートキャンプ場'),
+                    new CampGroundAddress('テスト県テスト市テスト町000'),
+                    new CampGroundPrice(1000),
+                    new CampGroundImage('https://example.com/test.jpg'),
+                    new CampGroundStatus('published'),
+                    new CampGroundLocation('mountain'),
+                    new CampGroundElevation(1000)
+                ),
+            ],
+            $repository->get($filter)
+        );
+    }
+}
