c11b9cb21a7b66e616506937f03dd5c0f8af8213
もっちゃん <s7056i3056@gmail.com>
2025-01-05 21:13:12 +0900

    add: factoryを使えるようにuseを追加

--- app/Models/CampGround.php
+++ app/Models/CampGround.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CampGround extends Model
 {
     use HasUlids;
+    use HasFactory;
 
     protected $fillable = [
         'name',
