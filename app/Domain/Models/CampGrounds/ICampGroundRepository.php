e5ff7ad22abfb05f866339c19bf9e24217a7718e
もっちゃん <s7056i3056@gmail.com>
2025-01-13 00:33:35 +0900

    refactor: unionのほうが馴染みがあるので書き換えた

--- app/Domain/Models/CampGrounds/ICampGroundRepository.php
+++ app/Domain/Models/CampGrounds/ICampGroundRepository.php
@@ -5,7 +5,7 @@ namespace App\Domain\Models\CampGrounds;
 interface ICampGroundRepository
 {
     public function get(GetCampGroundsFilter $filter): array;
-    public function findById(CampGroundId $id): ?CampGround;
+    public function findById(CampGroundId $id): CampGround | null;
     public function update(CampGround $camp_ground): CampGround;
     public function delete(CampGroundId $id): void;
 }
