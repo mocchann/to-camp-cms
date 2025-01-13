fb7dc154339e084d18e48244af8e7284ae5c0f1c
もっちゃん <s7056i3056@gmail.com>
2025-01-13 23:24:23 +0900

    feat: headerを作成

--- resources/js/pages/top/components/header.tsx
+++ resources/js/pages/top/components/header.tsx
@@ -5,10 +5,12 @@ import type { JSX } from 'react';
 
 export const Header = (): JSX.Element => {
   return (
-    <header className="flex items-center my-8">
-      <h1 className="justify-start">Header</h1>
-      <Button>Register</Button>
-      <Button>Login</Button>
+    <header className="flex items-center p-4">
+      <h1 className="justify-start font-bold">TO-CAMP-CMS</h1>
+      <div className="ml-auto flex space-x-4">
+        <Button>Register</Button>
+        <Button>Login</Button>
+      </div>
     </header>
   );
 };
