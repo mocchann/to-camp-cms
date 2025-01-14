b02b02de84cf822da5bf11159842df17914ba917
もっちゃん <s7056i3056@gmail.com>
2025-01-14 23:39:31 +0900

    chore: viteサーバーがlocalhostをリッスンしていてappコンテナからviteへの疎通ができなかったのでhostを設定した

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
   plugins: [
     laravel({
       input: [
