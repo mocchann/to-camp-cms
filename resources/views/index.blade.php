893222249d91a616e8e05599d7d90aa21ba8198d
もっちゃん <s7056i3056@gmail.com>
2025-01-15 23:50:09 +0900

    fix: hotまたはmanifest.jsonがなくてもエラーにならないように制御を入れた

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top</title>
+    @if(file_exists('./hot') || file_exists('./build/manifest.json'))
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/pages/top/index.tsx'])
+    @endif
 </head>
 
 <body>
