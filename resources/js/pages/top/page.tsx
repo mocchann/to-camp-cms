60e684f2cebf8da8c9d1f512c86d560a245eb79c
もっちゃん <s7056i3056@gmail.com>
2025-01-17 00:15:14 +0900

    feat: Basic Tableを作成

--- resources/js/pages/top/page.tsx
+++ resources/js/pages/top/page.tsx
@@ -1,7 +1,13 @@
-import { Table } from '@/components/ui/table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import type { JSX } from 'react';
 import { Header } from './components/header';
-import { Footer } from './components/footer';
 
 // type Props = {};
 
@@ -9,10 +15,56 @@ export const Page = (): JSX.Element => {
   return (
     <>
       <Header />
-      <main>
-        <Table />
+      <main className="container mx-auto py-10">
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>id</TableHead>
+                <TableHead>name</TableHead>
+                <TableHead>address</TableHead>
+                <TableHead>price</TableHead>
+                <TableHead>image</TableHead>
+                <TableHead>status</TableHead>
+                <TableHead>location</TableHead>
+                <TableHead>elevation</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>name</TableCell>
+                <TableCell>address</TableCell>
+                <TableCell>price</TableCell>
+                <TableCell>image</TableCell>
+                <TableCell>status</TableCell>
+                <TableCell>location</TableCell>
+                <TableCell>elevation</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>2</TableCell>
+                <TableCell>name</TableCell>
+                <TableCell>address</TableCell>
+                <TableCell>price</TableCell>
+                <TableCell>image</TableCell>
+                <TableCell>status</TableCell>
+                <TableCell>location</TableCell>
+                <TableCell>elevation</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>3</TableCell>
+                <TableCell>name</TableCell>
+                <TableCell>address</TableCell>
+                <TableCell>price</TableCell>
+                <TableCell>image</TableCell>
+                <TableCell>status</TableCell>
+                <TableCell>location</TableCell>
+                <TableCell>elevation</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
       </main>
-      <Footer />
     </>
   );
 };
