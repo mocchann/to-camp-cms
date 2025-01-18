76e266ac5d95198e57e150761eb13059a8c31766
もっちゃん <s7056i3056@gmail.com>
2025-01-18 18:10:06 +0900

    chore: mantineのセットアップ

--- resources/js/pages/top/index.tsx
+++ resources/js/pages/top/index.tsx
@@ -1,9 +1,19 @@
+import { createTheme, MantineProvider } from '@mantine/core';
 import { Page } from './page';
 import { createRoot } from 'react-dom/client';
 
 const domNode = document.getElementById('top');
 
+const theme = createTheme({
+  fontFamily: 'Open Sans, sans-serif',
+  primaryColor: 'blue',
+});
+
 if (domNode) {
   const root = createRoot(domNode);
-  root.render(<Page />);
+  root.render(
+    <MantineProvider theme={theme}>
+      <Page />
+    </MantineProvider>,
+  );
 }
