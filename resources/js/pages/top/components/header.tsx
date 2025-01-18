a873c891e22fa59e13c1644b6df628d479d3ccb6
もっちゃん <s7056i3056@gmail.com>
2025-01-18 12:28:31 +0900

    remove: shadcn/uiを使うのが本筋とずれて面倒なので一旦インストール前に戻した
    tailwindcssは使いそうな希ガスるので残し

--- resources/js/pages/top/components/header.tsx
+++ resources/js/pages/top/components/header.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import type { JSX } from 'react';
 
 // type Props = {};
@@ -8,8 +7,8 @@ export const Header = (): JSX.Element => {
     <header className="flex items-center p-4">
       <h1 className="justify-start font-bold">TO-CAMP-CMS</h1>
       <div className="ml-auto flex space-x-4">
-        <Button>Register</Button>
-        <Button>Login</Button>
+        <button type="button">Register</button>
+        <button type="button">Login</button>
       </div>
     </header>
   );
