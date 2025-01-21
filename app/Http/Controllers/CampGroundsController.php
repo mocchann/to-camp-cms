5189721c8166d392cf47871067c5fcd997bf888c
もっちゃん <s7056i3056@gmail.com>
2025-01-21 23:00:46 +0900

    refactor: コントローラー経由で表示

--- app/Http/Controllers/CampGroundsController.php
+++ app/Http/Controllers/CampGroundsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CampGroundsController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('camp_grounds.index');
+    }
 }
