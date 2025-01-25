d2d06e501e1bfcaefc56aea957ecca2274b2bcbd
もっちゃん <s7056i3056@gmail.com>
2025-01-25 23:49:04 +0900

    feat: キャンプ場更新ワークフローのコントローラーを作成

--- app/Http/Controllers/UpdateCampGroundController.php
+++ app/Http/Controllers/UpdateCampGroundController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCase\CampGrounds\UpdateCampGround;
+use App\UseCase\CampGrounds\UpdateCampGroundCommand;
 use Illuminate\Http\Request;
 
 class UpdateCampGroundController extends Controller
 {
-    //
+    public function update(Request $request, UpdateCampGround $use_case)
+    {
+        $command = new UpdateCampGroundCommand(
+            $request->id,
+            $request->name,
+            $request->address,
+            $request->price,
+            $request->image,
+            $request->status,
+            $request->location,
+            $request->elevation
+        );
+
+        $use_case->execute($command);
+
+        return redirect()->route('camp_grounds.index');
+    }
 }
