4cb21a060bdf1ae5cfee35136a04cb2236546477
もっちゃん <s7056i3056@gmail.com>
2025-01-27 23:40:49 +0900

    add: updateのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CampGroundsController;
+use App\Http\Controllers\UpdateCampGroundController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CampGroundsController::class, 'index']);
+Route::get('/update/{id}', [UpdateCampGroundController::class, 'index']);
