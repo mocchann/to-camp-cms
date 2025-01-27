b5aed55331bf414c1136ffccad3fc9ba20806ed0
もっちゃん <s7056i3056@gmail.com>
2025-01-27 23:41:32 +0900

    feat: indexメソッドを実装

--- app/Http/Controllers/UpdateCampGroundController.php
+++ app/Http/Controllers/UpdateCampGroundController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCase\CampGrounds\GetCampGround;
 use App\UseCase\CampGrounds\UpdateCampGround;
 use App\UseCase\CampGrounds\UpdateCampGroundCommand;
 use Illuminate\Http\Request;
 
 class UpdateCampGroundController extends Controller
 {
-    public function index()
+    public function index(string $id, GetCampGround $use_case)
     {
-        return view('update_camp_ground.index');
+        $camp_ground = $use_case->execute($id);
+
+        return view('update_camp_ground.index', ['camp_ground' => $camp_ground]);
     }
 
     public function update(Request $request, UpdateCampGround $use_case)
