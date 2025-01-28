2130464616a90a81396dd072cd97be82f4c890df
もっちゃん <s7056i3056@gmail.com>
2025-01-28 22:42:43 +0900

    add: createとupdateのルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\CampGroundsController;
+use App\Http\Controllers\CreateCampGroundController;
 use App\Http\Controllers\UpdateCampGroundController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CampGroundsController::class, 'index']);
+Route::get('/create', [CreateCampGroundController::class, 'index']);
+Route::post('/create', [CreateCampGroundController::class, 'create']);
 Route::get('/update/{id}', [UpdateCampGroundController::class, 'index']);
+Route::post('/update/{id}', [UpdateCampGroundController::class, 'update']);
