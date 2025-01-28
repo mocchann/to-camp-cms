37b00e9bfa74f1b04e97e507d46997796b76cb39
もっちゃん <s7056i3056@gmail.com>
2025-01-28 22:42:57 +0900

    feat: createのコントローラーを作成

---
+++ app/Http/Controllers/CreateCampGroundController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UseCase\CampGrounds\UpdateCampGround;
+use App\UseCase\CampGrounds\UpdateCampGroundCommand;
+use Illuminate\Http\Request;
+
+class CreateCampGroundController extends Controller
+{
+    public function index()
+    {
+        return view('create_camp_ground.index');
+    }
+
+    public function create(Request $request, UpdateCampGround $use_case)
+    {
+        $command = new UpdateCampGroundCommand(
+            $request->id,
+            $request->name,
+            $request->address,
+            $request->price,
+            $request->image,
+            $request->status,
+            $request->location,
+            $request->elevation
+        );
+
+        $use_case->execute($command);
+
+        return redirect()->route('camp_grounds.index');
+    }
+}
