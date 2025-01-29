3258ed90f289b4f2e1cf05836499f41034b8c119
もっちゃん <s7056i3056@gmail.com>
2025-01-29 23:53:50 +0900

    feat: キャンプ場登録ページを仮実装

--- resources/js/pages/create/page.tsx
+++ resources/js/pages/create/page.tsx
@@ -1,5 +1,68 @@
+import { Checkbox } from '@mantine/core';
 import type { JSX } from 'react';
+import {
+  Anchor,
+  AppShell,
+  Button,
+  Flex,
+  Group,
+  rem,
+  TextInput,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useHeadroom } from '@mantine/hooks';
 
 export const Page = (): JSX.Element => {
-  return <>create page</>;
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
+
+  const pinned = useHeadroom({ fixedAt: 120 });
+
+  return (
+    <AppShell
+      header={{ height: 60, collapsed: !pinned, offset: false }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Flex justify="space-between" align="center" my={12} mx={12}>
+          <Anchor>TO-CAMP-CMS</Anchor>
+          <div>
+            <Button>SignUp</Button>
+            <Button ml={12}>Login</Button>
+          </div>
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`}>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="your@email.com"
+            key={form.key('email')}
+            {...form.getInputProps('email')}
+          />
+
+          <Checkbox
+            mt="md"
+            label="I agree to sell my privacy"
+            key={form.key('termsOfService')}
+            {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+          />
+
+          <Group justify="flex-end" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </AppShell.Main>
+    </AppShell>
+  );
 };
