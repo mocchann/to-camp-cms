24f18527395e4a2d35e4c2095004486dcbe0c963
もっちゃん <s7056i3056@gmail.com>
2025-02-03 00:39:56 +0900

    fix: id

--- resources/js/components/Top/index.tsx
+++ resources/js/components/Top/index.tsx
@@ -2,7 +2,7 @@ import type { JSX } from 'react';
 import { Page } from './page';
 
 export const Top = (): JSX.Element => {
-  const campGroundsJson = document.getElementById('top')?.dataset.campGrounds;
+  const campGroundsJson = document.getElementById('root')?.dataset.campGrounds;
   const campGrounds = campGroundsJson ? JSON.parse(campGroundsJson) : [];
 
   return <Page campGrounds={campGrounds} />;
