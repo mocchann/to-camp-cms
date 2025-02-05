a84f94616f3868c4e8e0fdb54a74e3dcc16fd89c
もっちゃん <s7056i3056@gmail.com>
2025-02-05 22:55:40 +0900

    feat: routeヘルパーを使うためnameを設定

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CreateCampGroundController;
 use App\Http\Controllers\UpdateCampGroundController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CampGroundsController::class, 'index']);
+Route::get('/', [CampGroundsController::class, 'index'])->name('camp_grounds.index');
 Route::get('/create', [CreateCampGroundController::class, 'index']);
 Route::post('/create', [CreateCampGroundController::class, 'create']);
 Route::get('/update/{id}', [UpdateCampGroundController::class, 'index']);
