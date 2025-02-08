1573b43edfed695010dc955cfac4afb65f950fdb
もっちゃん <s7056i3056@gmail.com>
2025-02-08 23:52:53 +0900

    feat: updateページを過去のフォームに値を表示するまで実装した

--- resources/views/update_camp_ground/index.blade.php
+++ resources/views/update_camp_ground/index.blade.php
@@ -1,3 +1,30 @@
 @extends('main')
 
 @section('title', 'Update')
+
+@section('data-attributes')
+data-action="{{ action([App\Http\Controllers\UpdateCampGroundController::class, 'update'], ['id' => $camp_ground->getId()->getValue()]) }}"
+data-camp-ground="{{ json_encode(
+    [
+        'id'=> $camp_ground->getId()->getValue(),
+        'name' => $camp_ground->getName()->getValue(),
+        'address' => $camp_ground->getAddress()->getValue(),
+        'price' => $camp_ground->getPrice()->getValue(),
+        'image' => $camp_ground->getImage()->getValue(),
+        'status' => $camp_ground->getStatus()->getValue()->label(),
+        'location' => $camp_ground->getLocation()->getValue()->label(),
+        'elevation' => $camp_ground->getElevation()->getValue(),
+    ]
+  )
+}}"
+data-csrf-token="{{ csrf_token() }}"
+data-errors="{{ $errors->any() ? json_encode([
+    'name' => $errors->get('name') ?? null,
+    'address' => $errors->get('address') ?? null,
+    'price' => $errors->get('price') ?? null,
+    'image' => $errors->get('image') ?? null,
+    'status' => $errors->get('status') ?? null,
+    'location' => $errors->get('location') ?? null,
+    'elevation' => $errors->get('elevation') ?? null,
+  ]) : '' }}"
+@endsection
