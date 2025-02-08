9f337d71fc1cf30bd4f14cea9d9397148cb4cb60
もっちゃん <s7056i3056@gmail.com>
2025-02-08 23:52:22 +0900

    feat: updateページ表示

--- resources/js/components/Update/index.tsx
+++ resources/js/components/Update/index.tsx
@@ -2,5 +2,18 @@ import type { JSX } from 'react';
 import { Page } from './page';
 
 export const Update = (): JSX.Element => {
-  return <Page />;
+  const action = document.getElementById('root')?.dataset.action || '';
+  const csrfToken = document.getElementById('root')?.dataset.csrfToken || '';
+  const errors = document.getElementById('root')?.dataset.errors || undefined;
+  const campGroundJson = document.getElementById('root')?.dataset.campGround;
+  const campGround = campGroundJson ? JSON.parse(campGroundJson) : [];
+
+  return (
+    <Page
+      action={action}
+      csrfToken={csrfToken}
+      errors={errors}
+      campGround={campGround}
+    />
+  );
 };
