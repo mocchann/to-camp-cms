5929be9d57eb1eb569d0dac24e1c719a45dbbbe6
もっちゃん <s7056i3056@gmail.com>
2025-02-08 16:45:01 +0900

    feat: deleteするためにcsrfトークンを渡す

--- resources/js/components/Top/index.tsx
+++ resources/js/components/Top/index.tsx
@@ -4,6 +4,7 @@ import { Page } from './page';
 export const Top = (): JSX.Element => {
   const campGroundsJson = document.getElementById('root')?.dataset.campGrounds;
   const campGrounds = campGroundsJson ? JSON.parse(campGroundsJson) : [];
+  const csrfToken = document.getElementById('root')?.dataset.csrfToken || '';
 
-  return <Page campGrounds={campGrounds} />;
+  return <Page campGrounds={campGrounds} csrfToken={csrfToken} />;
 };
