92623c417f3b2f816082c0d78190668ac810eb4a
もっちゃん <s7056i3056@gmail.com>
2025-02-20 00:41:14 +0900

    feat: loginとregisterのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\CampGroundsController;
 use App\Http\Controllers\CreateCampGroundController;
 use App\Http\Controllers\DeleteCampGroundController;
+use App\Http\Controllers\LoginUserController;
+use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\UpdateCampGroundController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CampGroundsController::class, 'index'])->name('camp_grounds.index');
+Route::get('/register', [RegisterUserController::class, 'index']);
+Route::get('/login', [LoginUserController::class, 'index']);
 Route::get('/create', [CreateCampGroundController::class, 'index']);
 Route::post('/create', [CreateCampGroundController::class, 'create']);
 Route::get('/update/{id}', [UpdateCampGroundController::class, 'index']);
