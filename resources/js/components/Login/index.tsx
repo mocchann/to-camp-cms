44ac6ca472daa0049b0e405792c3b7e13dc83ea6
もっちゃん <s7056i3056@gmail.com>
2025-02-20 00:41:48 +0900

    feat: loginとregisterのぺーじ追加

---
+++ resources/js/components/Login/index.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from 'react';
+
+export const Login = (): JSX.Element => {
+  return (
+    <>
+      <div>Login</div>
+      <p>email</p>
+      <p>password</p>
+    </>
+  );
+};
---
+++ resources/js/components/Register/page.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from 'react';
+
+export const Page = (): JSX.Element => {
+  return (
+    <>
+      <div>register</div>
+      <p>email</p>
+      <p>password</p>
+    </>
+  );
+};
