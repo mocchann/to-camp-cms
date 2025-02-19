b2f05ab15e6d98098e5f5c59b992e5d21396982c
もっちゃん <s7056i3056@gmail.com>
2025-02-20 00:41:19 +0900

    feat: loginとregisterのコントローラー追加

---
+++ app/Http/Controllers/LoginUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LoginUserController
+{
+    public function index()
+    {
+        return view('user_login.index');
+    }
+}
---
+++ app/Http/Controllers/RegisterUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RegisterUserController
+{
+    public function index()
+    {
+        return view('user_register.index');
+    }
+}
