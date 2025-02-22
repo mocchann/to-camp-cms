476e65e3d45878b225493634cb732fd3221221ce
もっちゃん <s7056i3056@gmail.com>
2025-02-22 14:26:17 +0900

    feat: register action

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCase\Users\UserRegister;
+use Illuminate\Http\Request;
+
 class RegisterUserController
 {
     public function index()
     {
         return view('user_register.index');
+    }
+
+    public function store(Request $request, UserRegister $use_case)
+    {
+        $result = $use_case->execute(
+            $request->id,
+            $request->name,
+            $request->email,
+            $request->password
+        );
+
+        if ($result === false) {
+            return redirect()->route('user_register.index')->with('error', 'User already exists');
+        }
+
+        return redirect()->route('camp_grounds.index');
     }
 }
