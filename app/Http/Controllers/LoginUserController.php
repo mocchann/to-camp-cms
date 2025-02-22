0c7586d4c6fc1fc223fef996338e6deeb4bb713c
もっちゃん <s7056i3056@gmail.com>
2025-02-22 14:26:05 +0900

    feat: login Action

--- app/Http/Controllers/LoginUserController.php
+++ app/Http/Controllers/LoginUserController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCase\Users\UserLogin;
+use Illuminate\Http\Request;
+
 class LoginUserController
 {
     public function index()
     {
         return view('user_login.index');
+    }
+
+    public function store(Request $request, UserLogin $use_case)
+    {
+        $result = $use_case->execute($request->email, $request->password);
+
+        if ($result) {
+            return redirect()->route('camp_grounds.index');
+        }
+
+        return redirect()->route('user_login.index');
     }
 }
