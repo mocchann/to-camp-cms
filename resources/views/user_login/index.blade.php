864b3552b198b4b05a14c8b7dc955173226238f9
もっちゃん <s7056i3056@gmail.com>
2025-03-04 23:00:00 +0900

    feat: add validation message in blade

--- resources/views/user_login/index.blade.php
+++ resources/views/user_login/index.blade.php
@@ -6,4 +6,5 @@
 data-action="{{ action([App\Http\Controllers\LoginUserController::class, 'store']) }}"
 data-csrf-token="{{ csrf_token() }}"
 data-auth-check="{{ Auth::check() }}"
+data-error="{{ session('error') }}"
 @endsection
