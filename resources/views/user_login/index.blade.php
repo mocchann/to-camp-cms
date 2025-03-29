d972ac8e903e492199b3de803da73ea7f7a34954
もっちゃん <s7056i3056@gmail.com>
2025-03-29 23:52:56 +0900

    feat: login blade error

--- resources/views/user_login/index.blade.php
+++ resources/views/user_login/index.blade.php
@@ -7,4 +7,5 @@ data-action="{{ action([App\Http\Controllers\LoginUserController::class, 'store'
 data-csrf-token="{{ csrf_token() }}"
 data-auth-check="{{ Auth::check() }}"
 data-session-errors="{{ json_encode(session('error')) }}"
+data-errors="{{ json_encode($errors->messages()) }}"
 @endsection
